74fde751d6126bf06d7aa6d2310a92404cf384a0
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-01-02 09:24:21 +0100

    refactored a bunch of code, started better device handling (bufferPools and Queues), renamed stuff, added namespace

--- SwapChainHandler.hpp
+++ SwapChainHandler.hpp
@@ -5,119 +5,121 @@
 #include "VulkanFormatsHandler.hpp"
 #include "VulkanHandler.hpp"
 
-class SurfaceHandler
+namespace magma
 {
-public:
-  /// TODO: fix imperfection :(
-  VulkanHandler &vulkanHandler;
-  vk::SurfaceKHR vkSurface;
-
-  SurfaceHandler(VulkanHandler &vulkanHandler, vk::SurfaceKHR vkSurface)
-    : vulkanHandler(vulkanHandler)
-    , vkSurface(vkSurface)
+  class Surface
   {
-  }
+  public:
+    /// TODO: fix imperfection :( -- later, is this an imprefection?
+    Instance &instance;
+    vk::SurfaceKHR vkSurface;
 
-  ~SurfaceHandler()
+    Surface(Instance &instance, vk::SurfaceKHR vkSurface)
+      : instance(instance)
+      , vkSurface(vkSurface)
+    {
+    }
+
+    ~Surface()
+    {
+      instance.vkInstance.destroySurfaceKHR(vkSurface);
+    }
+
+    bool isQueueFamilySuitable(vk::PhysicalDevice physicalDevice, uint32_t queueIndex)
+    {
+      return physicalDevice.getSurfaceSupportKHR(queueIndex, vkSurface);
+    }
+  };
+
+  class SwapChain
   {
-    vulkanHandler.vkInstance.destroySurfaceKHR(vkSurface);
-  }
+  public:
+    Device &device;
+    vk::SwapchainKHR vkSwapchain;
 
-  bool isQueueFamilySuitable(vk::PhysicalDevice physicalDevice, uint32_t queueIndex)
-  {
-    return physicalDevice.getSurfaceSupportKHR(queueIndex, vkSurface);
-  }
-};
+    template<class CONTAINER>
+    static auto chooseImageFormat(Surface &surface, vk::PhysicalDevice physicalDevice, CONTAINER &&formatChoices)
+    {
+      auto formats(physicalDevice.getSurfaceFormatsKHR(surface.vkSurface));
 
-class SwapChainHandler
-{
-public:
-  VulkanDeviceHandler &deviceHandler;
-  vk::SwapchainKHR vkSwapchain;
+      if (formats[0].format == vk::Format::eUndefined)
+	{
+	  formats[0].format = vk::Format::eR8G8B8Srgb; // yay!
+	  return formats[0];
+	}
+      FormatGroup possibillities = ([&formatChoices](FormatGroup possibillities) {
+	  for (auto const &pick : formatChoices)
+	    {
+	      if (auto best = (possibillities & pick))
+		return best;
+	    }
+	  throw std::runtime_error("Vulkan Swapchain: No suitable format found.");
+	})(FormatGroup{containerView(formats.begin(), formats.end(), [](auto const &format)
+				     {
+				       return format.format;
+				     })});
+      return *std::find_if(formats.begin(), formats.end(), [&possibillities](auto const &format)
+			   {
+			     return possibillities[format.format];
+			   });
+    }
 
-  template<class CONTAINER>
-  static auto chooseImageFormat(SurfaceHandler &surfaceHandler, vk::PhysicalDevice physicalDevice, CONTAINER &&formatChoices)
-  {
-    auto formats(physicalDevice.getSurfaceFormatsKHR(surfaceHandler.vkSurface));
+    // TODO: refactor the fuck out of this.
+    SwapChain(Surface &surface, Device &device, vk::PhysicalDevice physicalDevice)
+      : device(device)
+    {
+      constexpr auto preferedFormatRanking =
+	{
+	  ((vulkanFormatGroups::R8G8B8 | vulkanFormatGroups::B8G8R8) & vulkanFormatGroups::Srgb),
+	  ((vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8) & vulkanFormatGroups::Srgb),
+	  (vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8 | vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8)
+	};
+      auto format(chooseImageFormat(surface, physicalDevice, preferedFormatRanking));      
+      auto capabilities(physicalDevice.getSurfaceCapabilitiesKHR(surface.vkSurface));
+      auto presentModes(physicalDevice.getSurfacePresentModesKHR(surface.vkSurface));
 
-    if (formats[0].format == vk::Format::eUndefined)
-      {
-	formats[0].format = vk::Format::eR8G8B8Srgb; // yay!
-	return formats[0];
-      }
-    FormatGroup possibillities = ([&formatChoices](FormatGroup possibillities) {
-	for (auto const &pick : formatChoices)
-	  {
-	    if (auto best = (possibillities & pick))
-	      return best;
-	  }
-	throw std::runtime_error("Vulkan Swapchain: No suitable format found.");
-      })(FormatGroup{containerView(formats.begin(), formats.end(), [](auto const &format)
-				   {
-				     return format.format;
-				   })});
-    return *std::find_if(formats.begin(), formats.end(), [&possibillities](auto const &format)
-			 {
-			   return possibillities[format.format];
-			 });
-  }
+      constexpr auto order = {vk::PresentModeKHR::eMailbox,
+			      vk::PresentModeKHR::eFifoRelaxed,
+			      vk::PresentModeKHR::eFifo};
+      auto resultIt(std::find_if(order.begin(), order.end(), [&presentModes](auto presentMode)
+				 {
+				   return (std::find(presentModes.begin(), presentModes.end(), presentMode) != presentModes.end());
+				 }));
+      if (resultIt == order.end())
+	throw std::runtime_error("Vulkan Swapchain: No suitable presentation mode found.");
+      auto presentMode(*resultIt);
 
-  // TODO: refactor the fuck out of this.
-  SwapChainHandler(SurfaceHandler &surfaceHandler, VulkanDeviceHandler &deviceHandler, vk::PhysicalDevice physicalDevice)
-    : deviceHandler(deviceHandler)
-  {
-    constexpr auto preferedFormatRanking =
-      {
-	((vulkanFormatGroups::R8G8B8 | vulkanFormatGroups::B8G8R8) & vulkanFormatGroups::Srgb),
-	((vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8) & vulkanFormatGroups::Srgb),
-	(vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8 | vulkanFormatGroups::R8G8B8A8 | vulkanFormatGroups::B8G8R8A8)
-      };
-    
-    auto format(chooseImageFormat(surfaceHandler, physicalDevice, preferedFormatRanking));      
-    auto capabilities(physicalDevice.getSurfaceCapabilitiesKHR(surfaceHandler.vkSurface));
-    auto presentModes(physicalDevice.getSurfacePresentModesKHR(surfaceHandler.vkSurface));
+      if (!(capabilities.supportedUsageFlags & vk::ImageUsageFlagBits::eTransferDst))
+	throw std::runtime_error("Vulkan Swapchain: Mising eTransferDst for clear operation.");
 
-    constexpr auto order = {vk::PresentModeKHR::eMailbox,
-			    vk::PresentModeKHR::eFifoRelaxed,
-			    vk::PresentModeKHR::eFifo};
-    auto resultIt(std::find_if(order.begin(), order.end(), [&presentModes](auto presentMode)
-			       {
-				 return (std::find(presentModes.begin(), presentModes.end(), presentMode) != presentModes.end());
-			       }));
-    if (resultIt == order.end())
-      throw std::runtime_error("Vulkan Swapchain: No suitable presentation mode found.");
-    auto presentMode(*resultIt);
+      vk::SwapchainCreateInfoKHR createInfo({},
+					    surface.vkSurface,
+					    std::min(std::max((std::size_t)capabilities.minImageCount, 3ul), (std::size_t)capabilities.maxImageCount - (std::size_t)!capabilities.maxImageCount),
+					    format.format,
+					    format.colorSpace,
+					    (capabilities.currentExtent == vk::Extent2D{0xFFFFFFFFu, 0xFFFFFFFFu} ? capabilities.maxImageExtent : capabilities.currentExtent), // choose current or biggest extent possible
+					    1,
+					    vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eColorAttachment,
+					    vk::SharingMode::eExclusive, // nest 2 params unused because eExclusive
+					    0, // unused
+					    nullptr, // unused
+					    capabilities.currentTransform,
+					    vk::CompositeAlphaFlagBitsKHR::eOpaque,
+					    presentMode,
+					    true,
+					    nullptr); // old swapchain
 
-    if (!(capabilities.supportedUsageFlags & vk::ImageUsageFlagBits::eTransferDst))
-      throw std::runtime_error("Vulkan Swapchain: Mising eTransferDst for clear operation.");
+      vkSwapchain = device.createSwapchainKHR(createInfo);
+    }
 
-    vk::SwapchainCreateInfoKHR createInfo({},
-					  surfaceHandler.vkSurface,
-					  std::min(std::max((std::size_t)capabilities.minImageCount, 3ul), (std::size_t)capabilities.maxImageCount - (std::size_t)!capabilities.maxImageCount),
-					  format.format,
-					  format.colorSpace,
-					  (capabilities.currentExtent == vk::Extent2D{0xFFFFFFFFu, 0xFFFFFFFFu} ? capabilities.maxImageExtent : capabilities.currentExtent), // choose current or biggest extent possible
-					  1,
-					  vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eColorAttachment,
-					  vk::SharingMode::eExclusive, // nest 2 params unused because eExclusive
-					  0, // unused
-					  nullptr, // unused
-					  capabilities.currentTransform,
-					  vk::CompositeAlphaFlagBitsKHR::eOpaque,
-					  presentMode,
-					  true,
-					  nullptr); // old swapchain
+    auto getImageIndex(vk::Semaphore sem, vk::Fence fence)
+    {
+      return device.acquireNextImageKHR(vkSwapchain, 20, sem, fence);
+    }
 
-    vkSwapchain = deviceHandler.device.createSwapchainKHR(createInfo);
-  }
-
-  auto getImageIndex(vk::Semaphore sem, vk::Fence fence)
-  {
-    return deviceHandler.device.acquireNextImageKHR(vkSwapchain, 20, sem, fence);
-  }
-
-  ~SwapChainHandler()
-  {
-    deviceHandler.device.destroySwapchainKHR(vkSwapchain);
-  }
+    ~SwapChain()
+    {
+      device.destroySwapchainKHR(vkSwapchain);
+    }
+  };
 };
