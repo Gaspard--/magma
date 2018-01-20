2de06cccb07ac54d3694c0edaff14e1f2f654854
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-01-20 23:16:26 +0100

    Semaphores done??? I thought there was more in the spec.

---
+++ Semaphore.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "magma/Device.hpp"
+
+namespace magma
+{
+  struct SemaphoreDeleter
+  {
+    Device<claws::NoDelete> device;
+
+    void operator()(vk::Semaphore const &fence) const
+    {
+      if (device)
+	device.destroySemaphore(fence);
+    }
+  };
+
+  template<class Deleter = SemaphoreDeleter>
+  using Semaphore = claws::Handle<vk::Semaphore, Deleter>;
+
+  inline auto DeviceImpl::createSemaphore() const
+  {
+    return Semaphore<>(SemaphoreDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createSemaphore({}));
+  }
+};
