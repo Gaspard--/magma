e90f08a3bab85a4dc0620b0bfab23d39c3e1ac5a
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-01-30 18:30:55 +0100

    buffer view

---
+++ BufferView.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "magma/Device.hpp"
+
+namespace magma
+{
+  struct BufferViewDeleter
+  {
+    Device<claws::NoDelete> device;
+
+    void operator()(vk::BufferView const &bufferView) const
+    {
+      if (device)
+	device.destroyBufferView(bufferView);
+    }
+  };
+  
+  template<class Deleter = BufferViewDeleter>
+  using BufferView = claws::Handle<vk::BufferView, Deleter>;
+
+  inline auto DeviceImpl::createBufferView(vk::BufferViewCreateFlags flags, claws::Handle<vk::Buffer, claws::NoDelete> buffer, vk::Format format, vk::DeviceSize offset, vk::DeviceSize size) const
+  {
+    return BufferView<>(BufferViewDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createBufferView({flags, buffer, format, offset, size}));
+  }
+};
