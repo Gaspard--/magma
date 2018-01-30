def2ee716945fc90281aea5507f0c635f545941f
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-01-30 17:42:11 +0100

    Added buffer support

---
+++ Buffer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "magma/Device.hpp"
+
+namespace magma
+{
+  struct BufferDeleter
+  {
+    Device<claws::NoDelete> device;
+
+    void operator()(vk::Buffer const &buffer) const
+    {
+      if (device)
+	device.destroyBuffer(buffer);
+    }
+  };
+  
+  template<class Deleter = BufferDeleter>
+  using Buffer = claws::Handle<vk::Buffer, Deleter>;
+
+  inline auto DeviceImpl::createBuffer(vk::BufferCreateFlags flags, vk::DeviceSize size, vk::BufferUsageFlags usage, std::vector<uint32_t> const &queueFamilies) const
+  {
+    return Buffer<>(BufferDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createBuffer({flags, size, usage, vk::SharingMode::eExclusive, static_cast<uint32_t>(queueFamilies.size()), queueFamilies.data()}));
+  }
+
+  inline auto DeviceImpl::createBuffer(vk::BufferCreateFlags flags, vk::DeviceSize size, vk::BufferUsageFlags usage) const
+  {
+    return Buffer<>(BufferDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createBuffer({flags, size, usage, vk::SharingMode::eConcurrent, 0, nullptr}));
+  }
+};
