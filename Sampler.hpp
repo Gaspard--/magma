cd51df5b3a25e4a1fb521d3476aca8b96f465d49
plean <jean.plancher@epitech.eu>
2018-03-02 02:09:52 +0100

    Sampler update

--- Sampler.hpp
+++ Sampler.hpp
@@ -18,9 +18,9 @@ namespace magma
   template<class Deleter = SamplerDeleter>
   using Sampler = claws::Handle<vk::Sampler, Deleter>;
 
-  inline auto DeviceImpl::createSampler(vk::SamplerCreateFlags flag, vk::Filter magFilter, vk::Filter minFilter, vk::SamplerMipmapMode mipmapMode, vk::SamplerAddressMode adressModeU, vk::SamplerAddressMode adressModeV, vk::SamplerAddressMode adressModeW, float mipLoadBias, vk::Bool32 anisotropyEnable, float maxAnisotropy, vk::Bool32 compareEnable, vk::CompareOp compareOp, float minLod, float maxLod, vk::BorderColor borderColor, vk::Bool32 unnormalizedCoordinates) const
+  inline auto DeviceImpl::createSampler(vk::Filter magFilter, vk::Filter minFilter, vk::SamplerMipmapMode mipmapMode, vk::SamplerAddressMode adressModeU, vk::SamplerAddressMode adressModeV, vk::SamplerAddressMode adressModeW, float mipLoadBias, vk::Bool32 anisotropyEnable, float maxAnisotropy, vk::Bool32 compareEnable, vk::CompareOp compareOp, float minLod, float maxLod, vk::BorderColor borderColor, vk::Bool32 unnormalizedCoordinates) const
   {
-    return Sampler<>(SamplerDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createSampler({flag,
+    return Sampler<>(SamplerDeleter{magma::Device<claws::NoDelete>(*this)}, vk::Device::createSampler({{},
 	    magFilter, minFilter, mipmapMode, adressModeU, adressModeV, adressModeW,  mipLoadBias, anisotropyEnable,
 	    maxAnisotropy, compareEnable, compareOp, minLod, maxLod, borderColor, unnormalizedCoordinates
 	    }));
